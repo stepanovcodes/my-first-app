b881ea1ffb63c2fd70019e58907e2e50b9022ecf
Alex Stepanov <stepanov.hw@gmail.com>
2023-11-02 08:23:22 -0600

    Do 27. Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {
+
+  }
 
 }
